f446690d0544e84bfe7e5b46c300ea436517e97e
Bruno Aguiar <marcenariacatarina@gmail.com>
2022-07-08 03:54:55 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,152 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '100',
+                nome: 'Alexandre de Arruda',
+                partido: 'Partido Democratas',
+                vice: 'Jaques Raimundo',
+                partidovice: 'Partido Liberal',
+                    fotos:[
+                        {url:'1.jpg', legenda: 'Governador'},
+                        {url:'2.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: '200',
+                nome: 'Ricardo Anastasia',
+                partido: 'Partido dos Trabalhadores',
+                vice: 'José Portela',
+                partidovice: 'Partido do Social Trabalhador',
+                    fotos:[
+                        {url:'2.webp', legenda: 'Governador'},
+                        {url:'3.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: '300',
+                nome: 'Camila Costa',
+                partido: 'Partido da Social Democracia',
+                vice: 'Raimundo Nonato',
+                partidovice: 'Partido Republicano',
+                fotos:[
+                    {url:'5.jpg', legenda: 'Governador'},
+                    {url:'4.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '9999',
+                nome: 'Coronel Esmael Pontes',
+                partido: 'Partido Novo',
+                vice: '---------',
+                partidovice: '---------',
+                fotos:[
+                    {url:'8.jpg', legenda: 'Deputado Estadual'},
+                ]
+            },
+            {
+                numero: '8888',
+                nome: 'Claudio Azevedo',
+                partido: 'Partido dos Socialistas',
+                vice: '---------',
+                partidovice: '---------',
+                fotos:[
+                    {url:'9.jpg', legenda: 'Prefeito'},
+                ]
+            },
+            {
+                numero: '7777',
+                nome: 'Leonardo Queiroz',
+                partido: 'Partido dos Capitalistas',
+                vice: '---------',
+                partidovice: '---------',
+                fotos:[
+                    {url:'10.jpg', legenda: 'Prefeito'},
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Humberto Camara',
+                partido: 'Partido da Republica',
+                vice: '---------',
+                partidovice: '---------',
+                fotos:[
+                    {url:'3.webp', legenda: 'Deputado Federal'},
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Creuza Capistrano',
+                partido: 'Partido da Direita Conservador',
+                vice: '---------',
+                partidovice: '---------',
+                fotos:[
+                    {url:'1.png', legenda: 'Deputada Federal'},
+                ]
+            },
+            {
+                numero: '33333',
+                nome: 'Jarbas Vieira',
+                partido: 'Partido da Social Democracia',
+                vice: '---------',
+                partidovice: '---------',
+                fotos:[
+                    {url:'4.webp', legenda: 'Deputado Federal'},
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Luis Inácio Lula da Silva',
+                partido: 'Partido dos Trabalhadores',
+                vice: 'Dilma Roussef',
+                partidovice: 'Partido do Movimento Democrata',
+                    fotos:[
+                        {url:'6.jpg', legenda: 'Presidente'},
+                        {url:'11.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '20',
+                nome: 'Jair Bolsonaro',
+                partido: 'Partido dos Liberais',
+                vice: 'Coronel Paulo Manoel',
+                partidovice: 'Partido Livre',
+                    fotos:[
+                        {url:'7.webp', legenda: 'Presidente'},
+                        {url:'12.webp', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '30',
+                nome: 'Natalia Silva',
+                partido: 'Partido da Social Democracia',
+                vice: 'Helio Saraiva',
+                partidovice: 'Partido Republicano',
+                fotos:[
+                    {url:'12.jpg', legenda: 'Presidente'},
+                    {url:'2.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
---
+++ readme.md
@@ -0,0 +1,4 @@
+<p>This code is a replica of the Brazilian digital ballot box used in the election of political candidates.
+</br>
+The Movie of the digital app:</p>
+<video src="movie.mp4" width="1200" height="1200" controls="controls" autoplay="autoplay" type="video/mp4"></video>
